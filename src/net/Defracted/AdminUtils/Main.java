692c9a24f7237a43c32dd2e41592e8809df431e4
runic-tears <51153795+runic-tears@users.noreply.github.com>
2020-04-07 19:05:16 +0400

    Кастомный метод загрузки команд в плагин, зато своё и короче!

--- src/net/Defracted/AdminUtils/Main.java
+++ src/net/Defracted/AdminUtils/Main.java
@@ -3,24 +3,29 @@ package net.Defracted.AdminUtils;
 import net.Defracted.AdminUtils.commands.God;
 import net.Defracted.AdminUtils.commands.Heal;
 import net.Defracted.AdminUtils.completers.HealAndGodCompleter;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 public class Main extends JavaPlugin {
     public List<UUID> playersInGodMode = new ArrayList<>();
 
+    private void loadCommand(String command, CommandExecutor Executor, TabCompleter Completer) {
+        Objects.requireNonNull(getCommand(command)).setExecutor(Executor);
+        Objects.requireNonNull(getCommand(command)).setTabCompleter(Completer);
+    }
+
     @Override
     public void onEnable() {
-        this.getServer().getPluginManager().registerEvents(new MainListener(this), this);
+        getServer().getPluginManager().registerEvents(new MainListener(this), this);
 
-        this.getCommand("heal").setExecutor(new Heal());
-        this.getCommand("heal").setTabCompleter(new HealAndGodCompleter());
-
-        this.getCommand("god").setExecutor(new God(this));
-        this.getCommand("god").setTabCompleter(new HealAndGodCompleter());
+        loadCommand("heal", new Heal(), new HealAndGodCompleter());
+        loadCommand("god", new God(this), new HealAndGodCompleter());
     }
 
     @Override
